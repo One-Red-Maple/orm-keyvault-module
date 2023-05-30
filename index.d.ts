35bf8e659066048cf6263ebe0a277fd392c3c52e
Agam-Deep-AJ <agam.deep@oneredmaple.com>
2023-05-30 13:20:04 -0400

    Added TypeScript declaration module

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'dotenv' {
+    export function config(): void;
+}
+
+declare module "@azure/identity" {
+    export class ClientSecretCredential {
+      constructor(tenantId: string, clientId: string, clientSecret: string, options?: { additionallyAllowedTenants: string });
+    }
+}
+
+declare module "@azure/keyvault-secrets" {
+    export class SecretClient {
+      constructor(vaultUrl: string, credential: any);
+      listPropertiesOfSecrets(): Promise<Array<{ name: string }>>;
+      getSecret(secretName: string): Promise<{ value: string }>;
+    }
+}
+
+declare module "keyvault-env" {
+    export async function copyKeysToEnv(): Promise<void>;
+}
